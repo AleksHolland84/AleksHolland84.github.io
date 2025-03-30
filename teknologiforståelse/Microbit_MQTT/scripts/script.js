909048d501e5da4d6a215dbfd5aa473575aede3a
AleksHolland84 <aleksholland@hotmail.com>
2025-03-30 17:20:26 +0200

    cleaned up code
    Cleaned up code
    removed unused css tags
    removed active from first menu when loaded
    added automatic numbering of headlines

--- teknologiforståelse/Microbit_MQTT/scripts/script.js
+++ teknologiforståelse/Microbit_MQTT/scripts/script.js
@@ -1,18 +1,4 @@
-function show_hide(){
-    //if the element 'appear' is set 'none' so the elemetnt is not visible
-    if (document.getElementById("appear").style.display == 'none') {
-      //make the element visible by setting the value of the property to 'block'.
-      document.getElementById("appear").style.display = 'block';
-    }else{
-      //ese if the element is visible hide the element by setting the value of the property to 'none'
-      document.getElementById("appear").style.display = 'none';
-    }
-  }
-
-
-
-  
-function showMenuContent() {
+ function showMenuContent() {
   var menues = document.getElementsByClassName("menu_text");
   var side = document.getElementsByClassName("menu_side");
   for (i = 0; i < menues.length; i++) {
@@ -30,7 +16,7 @@ function showMenuContent() {
 // Code By Webdevtrick ( https://webdevtrick.com )
 "use strict";
 var underlineMenuItems = document.querySelectorAll("ul.top li");
-underlineMenuItems[0].classList.add("active");
+//underlineMenuItems[0].classList.add("active"); // add active to class on first menu item
 underlineMenuItems.forEach(function (item) {
   item.addEventListener("click", function () {
       underlineMenuItems.forEach(function (item) { return item.classList.remove("active"); });
@@ -48,31 +34,18 @@ for (var i = 0; i < menuSides.length; i++) {
   });
 }
 
-// Function to get chapter title from menu and putting it as the header for each chapter.
+// The two functions below (getChapterTitle() & writeChaptreTitle()) gets the h2 chapters in the index file,
+// and adds chapter number in front - for automating "sorting" of new chapters or added content. 
 function getChapterTitle(chapterNum) {
 var chapters = document.getElementsByClassName("menu-chapter-title");
 return chapters[chapterNum].innerHTML;
 }
 
-
-function getChapterNumber(chapter) {
-  var chapterNum = document.getElementsByClassName("menu-chapter-number");
-  return chapterNum[chapter].innerHTML;
-}
-
 function writeChapterTitle(){
   var chapterTitle = document.getElementsByClassName("chapter-title");
   for (i = 0; i <chapterTitle.length; i++) {
-      chapterTitle[i].innerHTML = getChapterTitle(i);
+      chapterTitle[i].innerHTML = (i+1) +". "+ getChapterTitle(i);
   }
-}
-
-function writeChapterNumber(){
-  var chapterCounter = document.getElementsByClassName("chapterNum");
-  var menuChapterNumber = document.getElementsByClassName("chapterLink");
-  for (i = 0; i <chapterCounter.length; i++) {
-      chapterCounter[i].innerHTML = "Kapitel " + (i+1);
-  };
 }
 
 // FUNCTION TO GET FILENAME
@@ -86,37 +59,10 @@ function getFileName() {
   }
 }
 
-// SET LOCALSTORAGE WITH ALL CHECKED CHEKCBOXES
-function getChecked() {
-  const checked = []
-  var inputs = document.getElementsByTagName("input");
-  for(var i = 0; i < inputs.length; i++) {
-    if (inputs[i].type === "checkbox") {
-      const checkboxId = inputs[i].id;
-      const checkboxAnswer = inputs[i].checked
-      checked.push({id: checkboxId, answer: checkboxAnswer})
-    }
-  }
-  console.log(checked)
-  localStorage.setItem("checked", JSON.stringify(checked));
-}
-
-// GET ALL CHECKBOXES DATA FROM LOCALSTORAGE
-function setChecked(){
-  const checked = JSON.parse(localStorage.getItem("checked"));
-  console.log("From localStorage", checked)
-  for (var i = 0; i < checked.length; i++) {
-    const checkboxId = checked[i].id;
-    const checkboxAnswer = checked[i].answer;
-    document.getElementById(checkboxId).checked = checkboxAnswer;
-  }
-}
-
 
 
 function runOnLoad(){
   showMenuContent();
   writeChapterTitle();
-  writeChapterNumber();
   getFileName();
 }
